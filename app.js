3aad9d72641d008265fa1e028fd226cd0802ecf5
Ed <eduramq@gmail.com>
2023-05-09 00:15:11 -0700

    Using special variable event in vue method

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name:''
     };
   },
 
   methods : {
     // Whenever a function is listening to an event it does not need to return a value
+    setName(event) {
+
+      this.name = event.target.value ;
+    },
     add(num) {
       this.counter+=num;
     },
