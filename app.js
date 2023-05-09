7728947f27d017639f738c32d9c6322f65c7e3ff
Ed <eduramq@gmail.com>
2023-05-08 23:47:48 -0700

    Listening to js events using v-on directive

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+// document.querySelector('button').addEventListener();
+
 const app = Vue.createApp({
   data() {
     return {
       counter: 0,
     };
   },
+
+  methods : {
+    // Whenever a function is listening to an event it does not need to return a value
+    add() {
+      this.counter++;
+    },
+    reduce() {
+      this.counter--;
+    }
+  }
 });
 
 app.mount('#events');
